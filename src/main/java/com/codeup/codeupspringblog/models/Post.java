ad5c8ce2151e87d748f2a17ec2c798c3c6ab08dc
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-03-26 09:58:48 -0500

    repositories and jpa finished 1 through 8

--- src/main/java/com/codeup/codeupspringblog/models/Post.java
+++ src/main/java/com/codeup/codeupspringblog/models/Post.java
@@ -1,15 +1,23 @@
 package com.codeup.codeupspringblog.models;
 
 import lombok.*;
+import jakarta.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
+@Table(name="posts")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
+    @Column(nullable = false, length = 100)
     private String title;
+
+    @Column(nullable = false)
     private String body;
 
 }
